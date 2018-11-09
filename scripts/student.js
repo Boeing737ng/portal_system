cf9af706c7ace933f3ac56c0541673fd2fb1764a
Boeing737ng <ghlove0317@naver.com>
2018-11-09 21:22:42 +0900

    Required functions added

--- scripts/student.js
+++ scripts/student.js
@@ -5,5 +5,24 @@ class Student extends User {
         this.department = department
         this.student_num = student_num
         this.student_state = student_state
+        state_history = []
     }
+    // Getter
+    getName() { return this.name }
+    getType() { return this.type }
+    getDepartment() { return this.departmen }
+    getStudentNum() { return this.student_num }
+    getStudentState() { return this.student_state }
+
+    // Change student's current state
+    changeStudentState(new_state) {
+        this.student_state = new_state
+        this.saveStateChangedHistory(new_state)
+    }
+
+    // Saves student's state history
+    saveStateChangedHistory(new_state) {
+        state_history.push(new_state)
+    }
+
 }
