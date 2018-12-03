468212774533e7d9dfbeefff7839491b3b1ffabb
Boeing737ng <ghlove0317@naver.com>
2018-12-03 16:25:38 +0900

    Fetch user id,name for displaying

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,22 @@
-firebase.auth().onAuthStateChanged(function (user) {
+firebase.auth().onAuthStateChanged(function (user){
     if (user) {
-        userEmail = getSignInEmail();
-        if(userEmail === "admin@portal.com") {
-            firebase.database().ref().child('users/admin/').on('value', function(snapshot) {
-                document.getElementById('id-span').textContent = snapshot.val().id;
-                document.getElementById('user-type-span').textContent = snapshot.val().name;
-            })
+        var userEmail = getSignInEmail();
+        var id = userEmail.split('@')[0];
+        if(id === "admin") {
+            document.getElementById('id-span').textContent = 'admin';
+            document.getElementById('user-type-span').textContent = '학사 담당자';
         } else {
-            firebase.database().ref().child('users/' + userEmail + '/').on('value', function(snapshot) {
-                document.getElementById('id-span').textContent = snapshot.val().id;
-                document.getElementById('user-type-span').textContent = snapshot.val().name;
-            })
+            if(id.length == 6) {
+                firebase.database().ref().child('users/professor').on('value', function(snapshot) {
+                    document.getElementById('id-span').textContent = snapshot.val()[id].id;
+                    document.getElementById('user-type-span').textContent = snapshot.val()[id].name;
+                });
+            } else {
+                firebase.database().ref().child('users/student').on('value', function(snapshot) {
+                    document.getElementById('id-span').textContent = snapshot.val()[id].id;
+                    document.getElementById('user-type-span').textContent = snapshot.val()[id].name;
+                }); 
+            }
         }
     }
 });
